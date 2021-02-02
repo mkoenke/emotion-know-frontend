705c092633c8f379d8c0d8e43fabec332295f4b3
mkoenke <mkoenke@sas.upenn.edu>
2021-02-02 13:33:56 -0500

    add data averages before report creation

--- src/Components/RecordView.js
+++ src/Components/RecordView.js
@@ -1,10 +1,17 @@
+// import ReactMediaRecorder from "@getapper/react-media-recorder"
 import emailjs from "emailjs-com"
 import React from "react"
 import { connect } from "react-redux"
 import VideoRecorder from "react-video-recorder"
 import { Button, Form, Grid, Header } from "semantic-ui-react"
-// import { loadSDK } from "../../public/index.html"
-let CY
+
+let angerData = []
+let fearData = []
+let joyData = []
+let surpriseData = []
+let disgustData = []
+let sadnessData = []
+
 class RecordView extends React.Component {
   constructor(props) {
     super(props)
@@ -13,93 +20,36 @@ class RecordView extends React.Component {
       submittedTitle: "",
       videoBlob: null,
       emo: "",
+      emoData: "",
+      isRecording: false,
+      angerAvg: "",
+      fearAvg: "",
+      joyAvg: "",
+      surpriseAvg: "",
+      disgustAvg: "",
+      sadnessAvg: "",
     }
+    // if (document.getElementById("sdkvideo")) {
+    //   const customSource = CY.createSource.fromVideoElement(
+    //     document.getElementById("sdkvideo")
+    //   )
     CY.loader()
+      // .source(customSource)
       .addModule(CY.modules().FACE_EMOTION.name)
       .load()
       .then(({ start, stop }) => start())
-
     window.addEventListener(CY.modules().FACE_EMOTION.eventName, (evt) => {
-      this.setState({ emo: evt.detail.output.dominantEmotion })
+      this.setState({
+        emo: evt.detail.output.dominantEmotion,
+        emoData: evt.detail.output.rawEmotion,
+      })
+      if (this.state.isRecording) {
+        this.collectEmotionData(evt.detail.output.rawEmotion)
+      }
     })
+    // }
   }
 
-  componentDidMount() {
-    // const url = "https://ai-sdk.morphcast.com/v1.14/ai-sdk.js"
-    // const script = document.createElement("script")
-    // document.body.appendChild(script)
-    // return new Promise((resolve, reject) => {
-    //   script.onload = () => resolve(CY)
-    //   script.onerror = reject
-    //   script.src = url
-    // }).then((CY) => {
-    //   return (
-    //     CY.loader()
-    //       // .source(source)
-    //       .addModule(CY.modules().FACE_AROUSAL_VALENCE.name)
-    //       // .licenseKey('<KEY>')
-    //       .load()
-    //       .then((cmd) => {
-    //         this.cmd = cmd
-    //         cmd.start()
-    //         window.addEventListener("CY_FACE_AROUSAL_VALENCE_RESULT", (e) =>
-    //           this.TW.sendData(JSON.stringify(e.detail.output.arousalvalence))
-    //         )
-    //       })
-    //   )
-    // })
-    // class ScriptLoader {
-    //   static loadScript(url) {
-    //     return new Promise((resolve) => {
-    //       const script = document.createElement("script")
-    //       script.type = "text/javascript"
-    //       if (script.readyState) {
-    //         //IE
-    //         script.onreadystatechange = function () {
-    //           if (
-    //             script.readyState === "loaded" ||
-    //             script.readyState === "complete"
-    //           ) {
-    //             script.onreadystatechange = null
-    //             resolve()
-    //           }
-    //         }
-    //       } else {
-    //         //Others
-    //         script.onload = function () {
-    //           resolve()
-    //         }
-    //       }
-    //       script.src = url
-    //       document.getElementsByTagName("head")[0].appendChild(script)
-    //     })
-    //   }
-    //   static downloadAiSDK() {
-    //     if (ScriptLoader.p == null) {
-    //       ScriptLoader.p = ScriptLoader.loadScript(
-    //         "https://sdk.morphcast.com/mphtools/v1.0/mphtools.js"
-    //       )
-    //         .then(() =>
-    //           ScriptLoader.loadScript(
-    //             "https://ai-sdk.morphcast.com/v1.14/ai-sdk.js"
-    //           )
-    //         )
-    //         .then(() => CY) // CY is a global var
-    //     }
-    //     return ScriptLoader.p
-    //   }
-    // }
-    // ScriptLoader.downloadAiSDK().then((CY) => {
-    //   // here, the local variable CY can be changed to everything else (eg. AI)
-    //   CY.loader()
-    //     .addModule(CY.modules().FACE_DETECTOR.name)
-    //     .load()
-    //     .then(({ start, stop }) => start())
-    //   window.addEventListener(CY.modules().FACE_DETECTOR.eventName, (evt) => {
-    //     console.log("Face detector result", evt.detail)
-    //   })
-    // })
-  }
   changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value })
   }
@@ -171,6 +121,42 @@ class RecordView extends React.Component {
     //fetch reports
     // })
   }
+  getAverages = () => {
+    const average = (arr) => arr.reduce((sume, el) => sume + el, 0) / arr.length
+
+    let angerAvg = average(angerData)
+    let fearAvg = average(fearData)
+    let disgustAvg = average(disgustData)
+    let joyAvg = average(joyData)
+    let sadnessAvg = average(sadnessData)
+    let surpriseAvg = average(surpriseData)
+    this.setState({
+      angerAvg,
+      fearAvg,
+      disgustAvg,
+      joyAvg,
+      sadnessAvg,
+      surpriseAvg,
+    })
+  }
+
+  onRecordingComplete = (videoBlob) => {
+    this.setState({ videoBlob, isRecording: false }, this.getAverages)
+  }
+  onStartRecording = () => {
+    this.setState({ isRecording: true })
+  }
+
+  collectEmotionData = (emotionObj) => {
+    console.log("Emo Obj in Collection: ", emotionObj)
+
+    angerData = [...angerData, emotionObj.Angry]
+    fearData = [...fearData, emotionObj.Fear]
+    disgustData = [...fearData, emotionObj.Disgust]
+    joyData = [...fearData, emotionObj.Happy]
+    sadnessData = [...fearData, emotionObj.Sad]
+    surpriseData = [...fearData, emotionObj.Surprise]
+  }
 
   render() {
     console.log("State: ", this.state)
@@ -202,19 +188,36 @@ class RecordView extends React.Component {
             <video
               id="sdkvideo"
               muted
-              playsinline
+              playsInline
               style={{ position: "absolute", width: "0px", height: "0px" }}
             ></video>
             <div style={{ height: "620px", width: "800px" }}>
               <VideoRecorder
                 showReplayControls={true}
                 replayVideoAutoplayAndLoopOff={true}
-                onRecordingComplete={(videoBlob) => {
-                  this.setState({ videoBlob })
-                }}
+                onRecordingComplete={this.onRecordingComplete}
+                onStartRecording={this.onStartRecording}
               />
-
-              <div></div>
+              <Button onClick={this.getAverages}></Button>
+              {/* <div>
+                <ReactMediaRecorder
+                  video
+                  render={({
+                    status,
+                    startRecording,
+                    stopRecording,
+                    mediaBlob,
+                    mediaUrl,
+                  }) => (
+                    <div>
+                      <p>{status}</p>
+                      <button onClick={startRecording}>Start Recording</button>
+                      <button onClick={stopRecording}>Stop Recording</button>
+                      <video id="sdkvideo" src={mediaUrl} controls />
+                    </div>
+                  )}
+                />
+              </div> */}
             </div>
           </Grid>
           <div
@@ -240,3 +243,78 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(RecordView)
+
+// const url = "https://ai-sdk.morphcast.com/v1.14/ai-sdk.js"
+// const script = document.createElement("script")
+// document.body.appendChild(script)
+// return new Promise((resolve, reject) => {
+//   script.onload = () => resolve(CY)
+//   script.onerror = reject
+//   script.src = url
+// }).then((CY) => {
+//   return (
+//     CY.loader()
+//       // .source(source)
+//       .addModule(CY.modules().FACE_AROUSAL_VALENCE.name)
+//       // .licenseKey('<KEY>')
+//       .load()
+//       .then((cmd) => {
+//         this.cmd = cmd
+//         cmd.start()
+//         window.addEventListener("CY_FACE_AROUSAL_VALENCE_RESULT", (e) =>
+//           this.TW.sendData(JSON.stringify(e.detail.output.arousalvalence))
+//         )
+//       })
+//   )
+// })
+// class ScriptLoader {
+//   static loadScript(url) {
+//     return new Promise((resolve) => {
+//       const script = document.createElement("script")
+//       script.type = "text/javascript"
+//       if (script.readyState) {
+//         //IE
+//         script.onreadystatechange = function () {
+//           if (
+//             script.readyState === "loaded" ||
+//             script.readyState === "complete"
+//           ) {
+//             script.onreadystatechange = null
+//             resolve()
+//           }
+//         }
+//       } else {
+//         //Others
+//         script.onload = function () {
+//           resolve()
+//         }
+//       }
+//       script.src = url
+//       document.getElementsByTagName("head")[0].appendChild(script)
+//     })
+//   }
+//   static downloadAiSDK() {
+//     if (ScriptLoader.p == null) {
+//       ScriptLoader.p = ScriptLoader.loadScript(
+//         "https://sdk.morphcast.com/mphtools/v1.0/mphtools.js"
+//       )
+//         .then(() =>
+//           ScriptLoader.loadScript(
+//             "https://ai-sdk.morphcast.com/v1.14/ai-sdk.js"
+//           )
+//         )
+//         .then(() => CY) // CY is a global var
+//     }
+//     return ScriptLoader.p
+//   }
+// }
+// ScriptLoader.downloadAiSDK().then((CY) => {
+//   // here, the local variable CY can be changed to everything else (eg. AI)
+//   CY.loader()
+//     .addModule(CY.modules().FACE_DETECTOR.name)
+//     .load()
+//     .then(({ start, stop }) => start())
+//   window.addEventListener(CY.modules().FACE_DETECTOR.eventName, (evt) => {
+//     console.log("Face detector result", evt.detail)
+//   })
+// })
